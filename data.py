2e0a19e7e93e7c5f273153683a59c4f046975978
Lance Pery <lpery@academic.rrc.ca>
2026-06-04 11:21:53 -0500

    downloaded data into data file.

--- data.py
+++ data.py
@@ -1,6 +1,25 @@
 import kagglehub
+import shutil
+import os
 
-# Download latest version
-path = kagglehub.dataset_download("vivek468/superstore-dataset-final")
+# Download dataset to KaggleHub cache
+download_path = kagglehub.dataset_download(
+    "vivek468/superstore-dataset-final"
+)
 
-print("Path to dataset files:", path)
+print("Downloaded to:", download_path)
+
+# Your project data folder
+destination = r"D:\Projects_Python\Superstore_EDA\data"
+
+os.makedirs(destination, exist_ok=True)
+
+# Copy all files
+for file in os.listdir(download_path):
+    src = os.path.join(download_path, file)
+    dst = os.path.join(destination, file)
+
+    if os.path.isfile(src):
+        shutil.copy2(src, dst)
+
+print("Files copied to:", destination)
