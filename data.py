72ea0f08a91df03d4742527b0b5ab15b4eb655b9
Lance Pery <lpery@academic.rrc.ca>
2026-06-04 11:24:19 -0500

    now can read the data.

--- data.py
+++ data.py
@@ -1,25 +1,8 @@
-import kagglehub
-import shutil
-import os
+import pandas as pd
 
-# Download dataset to KaggleHub cache
-download_path = kagglehub.dataset_download(
-    "vivek468/superstore-dataset-final"
+df = pd.read_csv(
+    "data/Sample - Superstore.csv",
+    encoding="latin1"
 )
 
-print("Downloaded to:", download_path)
-
-# Your project data folder
-destination = r"D:\Projects_Python\Superstore_EDA\data"
-
-os.makedirs(destination, exist_ok=True)
-
-# Copy all files
-for file in os.listdir(download_path):
-    src = os.path.join(download_path, file)
-    dst = os.path.join(destination, file)
-
-    if os.path.isfile(src):
-        shutil.copy2(src, dst)
-
-print("Files copied to:", destination)
+print(df.head())
