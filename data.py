b13f60cc55c431a91ab5c10e583a10ddbf47a108
Lance Pery <lpery@academic.rrc.ca>
2026-06-05 15:12:20 -0500

    checks duplicates and missing values. There is none.

--- data.py
+++ data.py
@@ -5,9 +5,13 @@ df = pd.read_csv(
     encoding="latin1"
 )
 
-print("Shape: ", df.shape)
+# Display basic information about the dataset
+# print("Shape: ", df.shape)
+# print("\nColumns: ", df.columns.tolist())
+# print("\nData Types: ", df.dtypes)
+# print("\nMissing Values: ", df.isnull().sum())
+# print("\nSummary Statistics: \n", df.describe())
 
-print("\nColumns: ", df.columns.tolist())
-print("\nData Types: ", df.dtypes)
-print("\nMissing Values: ", df.isnull().sum())
-print("\nSummary Statistics: \n", df.describe())
+# Cleaning and preprocessing
+print("Duplicates: ", df.duplicated().sum())
+print("Missing Values: ", df.isnull().sum())
