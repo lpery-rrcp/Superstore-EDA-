d4742a16d2b64bcd6163f5b96d71785c10ff487a
Lance Pery <lpery@academic.rrc.ca>
2026-06-07 16:15:13 -0500

    added the sales total by sub category.

--- dashboard.py
+++ dashboard.py
@@ -24,4 +24,25 @@ def getSalesByCategory():
     plt.show()
 
 
+def getSalesBySubCategory():
+    sales_by_sub_category = (
+        df.groupby('Sub-Category')['Sales']
+        .sum()
+        .sort_values(ascending=False)
+    )
+
+    sales_by_sub_category.plot(
+        kind='bar', color=['#1f77b4', '#ff7f0e', '#2ca02c'])
+
+    plt.title('Sales by Sub-Category')
+    plt.xlabel('Sub-Category')
+    plt.ylabel('Total Sales')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    plt.show()
+
+
+# Test the function
 getSalesByCategory()
+getSalesBySubCategory()
